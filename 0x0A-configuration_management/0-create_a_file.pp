96792543e5c29e95b088f3e5de2ff808bb456958
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-07-01 01:39:57 +0300

    1t

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
 #creat_file
-file {'/tmp/school':
-        path  => '/tmp/school',
-        content => "I love Puppet",
-        owner   => 'www-data',     
-        group   => 'www-data',                   
-        mode => '0744',
+
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
     }
+    
